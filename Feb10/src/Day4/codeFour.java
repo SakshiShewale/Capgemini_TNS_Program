e67277e3b9d9da31c18bc4d870870e2986521cce
SakshiShewale <115781748+SakshiShewale@users.noreply.github.com>
2023-02-10 14:13:23 +0530

    Add files via upload

---
+++ Feb10/src/Day4/codeFour.java
@@ -0,0 +1,18 @@
+package Day4;
+
+public class codeFour {
+	
+	int a=45;
+	String r ="Sarika";
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		codeFour b= new codeFour();
+		
+		System.out.println(b.a);
+		System.out.println(b.r);
+
+	}
+
+}
