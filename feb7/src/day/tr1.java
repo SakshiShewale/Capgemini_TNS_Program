e3f56345b58aaf2a87756880ccc097a236e32790
SakshiShewale <115781748+SakshiShewale@users.noreply.github.com>
2023-02-10 14:07:24 +0530

    Add files via upload

---
+++ feb7/src/day/tr1.java
@@ -0,0 +1,24 @@
+package day;
+
+public class tr1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+String d="maharashtra";
+System.out.println(d.endsWith("tra"));
+
+System.out.println(d.endsWith("ta"));
+System.out.println(d.endsWith("a"));
+System.out.println(d.endsWith("s"));
+
+
+String s="ranjangoan";
+System.out.println(s);
+
+System.out.println(s.startsWith("mha"));
+System.out.println(s.startsWith("r"));
+System.out.println(s.startsWith("ran"));
+
+	}
+
+}
---
+++ feb7/src/day/trr1.java
@@ -0,0 +1,13 @@
+package day;
+
+public class trr1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+String s="government";
+System.out.println(s);
+String v= s.replace('g', 'r');
+System.out.println(v);
+	}
+
+}
