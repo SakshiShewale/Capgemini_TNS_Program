764445e9cd371ffac665b7a51464c4d9fc756cb5
SakshiShewale <115781748+SakshiShewale@users.noreply.github.com>
2023-02-16 14:54:20 +0530

    Add files via upload

---
+++ Feb16/src/day/Rules.java
@@ -0,0 +1,23 @@
+package day;
+
+public class Rules {
+	
+	int suspected = 60;
+}
+	class selto extends Rules
+	{
+		int suspected=170;
+		void disp() 
+		{
+			System.out.println("speed is "+super.suspected);
+		}
+	
+
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+	}
+	
+}
---
+++ Feb16/src/day/nashik_rules.java
@@ -0,0 +1,11 @@
+package day;
+
+public class nashik_rules extends selto{
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		selto obj = new selto ();
+		obj.disp();
+	}
+
+}
